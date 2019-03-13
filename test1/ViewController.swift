c80022462588dbe32a645d343034d87cd31a807d
D7703_11 <d7703_11@d7703-11ui-imac.local>
2019-03-13 14:26:04 +0900

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by D7703_11 on 2019. 3. 13..
+//  Copyright © 2019년 dit.ac.kr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
